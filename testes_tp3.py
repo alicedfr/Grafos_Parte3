024cafc7bc879591946935b31e8bb9dd50266f2f
Alice Duarte F Ribeiro <108374759+alicedfr@users.noreply.github.com>
2023-11-22 12:06:23 -0300

    Update testes_tp3.py

--- testes_tp3.py
+++ testes_tp3.py
@@ -1,20 +1,13 @@
 #Código usado para fazer os estudos de caso
 
-from TP3 import *
+def Ford_Fulkerson(grafo: str, d: bool, s, t, disco):
+        with open(f'{grafo}.txt', 'r') as arquivo:  #mudar o número do grafo de acordo com o teste 
+            texto = [vertice for vertice in arquivo.read().split()] #Cria uma lista com o conteúdo presente no arquivo de teste
+            vertices = int(texto[0]) #Pega o primeiro elemento da lista acima, que correnspode ao número de vértices do grafo
+            arestas = [] #Lista vazia que vai conter todos os pares de arestas do grafo
+            for i in range(len(texto[1:])//3):
+                arestas += [[int(texto[3*i+1])]+[int(texto[3*i+2])]+ [float(texto[3*i+3])]] 
+        
+        g = Graph_l(vertices, arestas, d)
+        g.FF(s, t, grafo, disco)
 
-with open('grafo_rf_1.txt', 'r') as arquivo:  #mudar o número do grafo de acordo com o teste 
-        texto = [vertice for vertice in arquivo.read().split()] #Cria uma lista com o conteúdo presente no arquivo de teste
-        vertices = int(texto[0]) #Pega o primeiro elemento da lista acima, que correnspode ao número de vértices do grafo
-        arestas = [] #Lista vazia que vai conter todos os pares de arestas do grafo
-        for i in range(len(texto[1:])//3):
-            arestas += [[int(texto[3*i+1])]+[int(texto[3*i+2])]+ [float(texto[3*i+3])]] 
-
-grafo = Graph_l(vertices, arestas, True)
-#print(grafo.mostra_lista())
-print(grafo.FF(1,2, 'grafo_rf_1', True))
-#print(grafo.get_caminho(1,6))
-#print(grafo.get_gargalo([6, 5, 3, 1]))
-#print(grafo.BFS_ff(1))
-
-
-#Estudo de caso:
