c4164842c23193634e0f948cddd102efcc12c4dc
Alice Duarte F Ribeiro <108374759+alicedfr@users.noreply.github.com>
2023-11-22 12:00:56 -0300

    Update classAresta.py

--- classAresta.py
+++ classAresta.py
@@ -1,5 +1,5 @@
 class Aresta:
-    def __init__(self, v1: int, v2: int, c, f, res: bool):
+    def _init_(self, v1: int, v2: int, c, f, res: bool):
         self.v1 = v1
         self.v2 = v2
         self.cap = c
@@ -9,10 +9,13 @@ class Aresta:
             self.cap_residual = self.fluxo
         else:
             self.cap_residual = self.cap - self.fluxo
-    
+          
+        self.opointer = None
+        self.rpointer = None
     def atualiza_arestas(self, gargalo):
         self.fluxo += gargalo
         if self.residual == True:
             self.cap_residual = self.fluxo
         else:
             self.cap_residual = self.cap - self.fluxo
+            self.rpointer.atualiza_arestas(gargalo)
